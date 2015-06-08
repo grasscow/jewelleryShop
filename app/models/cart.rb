885c3de03111ea075cf1a9e01f3267de30eec736
grasscow <dr.beat@inbox.ru>
2015-06-08 09:15:15 +0300

    associations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,4 @@
 class Cart < ActiveRecord::Base
+  has_one :order, dependent: :destroy
+  has_many :line_items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :addresses
+  has_many :addresses, dependent: :nullify
+  has_many :orders, dependent: :nullify
 end
